11ad3897a064185846e2e7876f1add56ca48472d
jenna-graham <jclayto2@gmail.com>
2022-04-20 13:02:34 -0700

    starting About me

--- app.js
+++ app.js
@@ -1,8 +1,19 @@
 // import functions
 
 // grab DOM elements
-
-// set event listeners 
-    // get user input
-    // use user input to update state 
-    // update DOM to reflect the new state
+const button = document.getElementById("button");
+const nameEl = document.getElementById("name");
+const sectionEl = document.getElementById("my-info");
+// set event listeners
+button.addEventListener("click", () => {
+    if (nameEl.textContent === '____' {
+        nameEl.textContent = 'Jenna'; 
+       else {
+           nameEl.textContent = '____';
+       }
+    })
+  sectionEl.classList.toggle("my-style");
+});
+// get user input
+// use user input to update state
+// update DOM to reflect the new state
